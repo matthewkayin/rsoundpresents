c798c128120e34b157070da7b6790da446aac557
Matt Madden <matthewkayin@gmail.com>
2018-04-01 14:06:44 -0500

    Finished site rework
    Signed-off-by: Matt Madden <matthewkayin@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
-window.onscroll = function() {
-    var el = document.getElementsByClassName('nav-button')[0];
-    var el2 = document.getElementById('site-header');
-    var className = 'small';
-    if (el.classList) {
+window.onload = function(){
+
+    correctNavPane();
+};
+
+window.onscroll = function(){
+
+    correctNavPane();
+};
+
+function correctNavPane(){
+
+    var header = document.getElementById('site-header');
+    if (header.classList) {
+
         if (window.scrollY > 10){
-            el.style.padding = '2px 10px';
-            el2.style.padding = '2px 0px';
+
+            header.classList.remove('larger');
+            header.classList.add('smaller');
+
+            for(i = 0; i < 4; i++){
+
+                var navButton = document.getElementsByClassName('nav-button')[i];
+                navButton.classList.add('smaller');
+                navButton.classList.remove('larger');
+            }
+
         }else{
-            el.style.padding = '10px 10px';
-            el2.style.padding = '10px 0px';
+
+            header.classList.remove('smaller');
+            header.classList.add('larger');
+
+            for(i = 0; i < 4; i++){
+
+                var navButton = document.getElementsByClassName('nav-button')[i];
+                navButton.classList.add('larger');
+                navButton.classList.remove('smaller');
+            }
         }
     }
-};
+}
